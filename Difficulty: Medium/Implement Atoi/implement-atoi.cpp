9fbe52463d823b08e77eb5db1e9060dce4b41d02
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2026-04-16 09:33:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string &s) {
+        int i = 0, n = s.size();
+        
+        // 1. Skip leading whitespaces
+        while (i < n && s[i] == ' ')
+            i++;
+        
+        // 2. Check sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+        
+        // 3. Convert digits
+        long result = 0;  // use long to detect overflow
+        
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            
+            result = result * 10 + digit;
+            
+            // 4. Handle overflow
+            if (sign * result > INT_MAX)
+                return INT_MAX;
+            if (sign * result < INT_MIN)
+                return INT_MIN;
+            
+            i++;
+        }
+        
+        return (int)(sign * result);
+    }
+};
